05a6063c450b4f5f0bf95f7cbf1dbffdb361280e
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-27 23:38:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,9 @@
 def oxford_comma(array)
-  if array.count == 1 
-    array.join
-  elsif array.count == 2
-    array.join(" and ")
-  elsif array.count == 3   #["kiwi", "durian", "starfruit"]
-    array[0..2].join(", ") + " and " + array.pop
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
-  array.join
-#  if array.length == 2
- #   return "#{array[0]} and #{array[1]}"
-  #elsif 2 < array.length
-   # array[-1].insert(0, "and ")
-  #end
-  #array.join(", ")
+  array.join(", ")
 end
 
