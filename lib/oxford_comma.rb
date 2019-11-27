89211982155c984a1c59790118aebe117058794e
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-27 23:34:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.count == 2
     array.join(" and ")
-  elsif array.count >= 3   #["kiwi", "durian", "starfruit"]
+  elsif array.count == 3   #["kiwi", "durian", "starfruit"]
     array[0, 1].join(", ") + " and " + array.pop
   end
   array.join
