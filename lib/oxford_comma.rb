038363d262cc2c3169d53cf552475ad91c18f19d
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-27 23:30:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,13 @@
 def oxford_comma(array)
-  if array.count == 1 
-    array.join
-  elsif array.count == 2
-    array.join(" and ")
-  elsif array.count >= 3   #["kiwi", "durian", "starfruit"]
-    array[-1].insert(0, "and ")
+  #if array.count == 1 
+   # array.join
+  #elsif array.count == 2
+   # array.join(" and ")
+  #elsif array.count >= 3   #["kiwi", "durian", "starfruit"]
+    #array[-1].insert(0, "and ")
     #array[0..2].join(", ") + " and " + array.pop
-    array.join
-  end
+    #array.join
+  #end
+  
 end
 
