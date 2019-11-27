ad6ff4d88b89fb99f147a820fefc9cc1b0edc212
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-27 23:31:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,11 @@ def oxford_comma(array)
     #array[0..2].join(", ") + " and " + array.pop
     #array.join
   #end
-  
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
 
