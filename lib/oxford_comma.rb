1f8b4cc5c1e6bc31056ae19506bdcf7b60470ded
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-27 23:08:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1 
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count == 3   #["kiwi", "durian", "starfruit"]
+    array[0..2].join(", ") + " and " + array.pop
+  end
 end
+
